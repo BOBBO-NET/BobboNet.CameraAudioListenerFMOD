bcde3ea3b239df3a2a4a48cd72d45fce7a54a5da
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-26 01:47:27 -0500

    Initial Commit

---
+++ Runtime/CameraAudioListenerFMOD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+namespace BobboNet.CameraAudioListener.FMOD
+{
+    public class CameraAudioListenerFMOD : ICameraAudioListener
+    {
+        public void Setup(Camera mainCamera)
+        {
+            mainCamera.gameObject.AddComponent<StudioListener>();
+        }
+    }
+
+}
